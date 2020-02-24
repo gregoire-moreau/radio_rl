d13af16e58845f9bd4e6d3c1421fd1fe130ca228
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-02-24 20:22:41 +0100

    Update tests_studssh.sh

--- tests_studssh.sh
+++ tests_studssh.sh
@@ -3,6 +3,6 @@
 lrs=(0.001 0.0001 0.00001 0.000001)
 
 for lr in ${lrs[@]}; do
-  ./main.py -n DQN -r dose -s c++ -l $lr 0.8 1 > training_logs/canicula_types_$lr 2>/dev/null
-  ./main.py --obs_type head -n DQN -r dose -s c++ -l $lr 0.8 1 > training_logs/canicula_head_$lr 2>/dev/null
+  ./main.py -n DQN -r dose -s c++ -l $lr 0.8 1 > training_logs/studssh_types_$lr 2>/dev/null
+  ./main.py --obs_type head -n DQN -r dose -s c++ -l $lr 0.8 1 > training_logs/studssh_head_$lr 2>/dev/null
 done
