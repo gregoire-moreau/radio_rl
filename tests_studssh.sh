2a4b7a0bcd62b5cce10cd742aa92f7e6d0677b92
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-04-23 01:56:58 +0200

    experiments

--- tests_studssh.sh
+++ tests_studssh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p training_logs/studssh
-./main.py -n AC --obs_type head -r dose -e 20 2500 > training_logs/studssh/head_reg
-./main.py -n AC --obs_type types -r dose -e 20 2500 > training_logs/studssh/types_reg
-./main.py -n AC --obs_type head -r dose -e 20 2500 --center > training_logs/studssh/head_center
-./main.py -n AC --obs_type types -r dose -e 20 2500 --center > training_logs/studssh/types_center
-./main.py -n AC --obs_type head -r dose -e 20 2500 -t > training_logs/studssh/head_radius
-./main.py -n AC --obs_type types -r dose -e 20 2500 -t > training_logs/studssh/types_radius
-./main.py -n AC --obs_type head -r dose -e 20 2500 --center -t > training_logs/studssh/head_both
-./main.py -n AC --obs_type types -r dose -e 20 2500 --center -t > training_logs/studssh/types_both
+mkdir -p training_logs/center
+./main.py -n AC --obs_type head -r dose -e 20 2500 > training_logs/center/head_reg
+./main.py -n AC --obs_type types -r dose -e 20 2500 > training_logs/center/types_reg
+./main.py -n AC --obs_type head -r dose -e 20 2500 --center > training_logs/center/head_center
+./main.py -n AC --obs_type types -r dose -e 20 2500 --center > training_logs/center/types_center
+./main.py -n AC --obs_type head -r dose -e 20 2500 -t > training_logs/center/head_radius
+./main.py -n AC --obs_type types -r dose -e 20 2500 -t > training_logs/center/types_radius
+./main.py -n AC --obs_type head -r dose -e 20 2500 --center -t > training_logs/center/head_both
+./main.py -n AC --obs_type types -r dose -e 20 2500 --center -t > training_logs/center/types_both
