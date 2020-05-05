a3681c4c01b6c8de005c2640c1e35f14c0f4443b
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-05-05 15:23:41 +0200

    cani

--- tests_canicula.sh
+++ tests_canicula.sh
@@ -2,4 +2,4 @@
 
 
 mkdir -p training_logs/cani
-./main.py --canicula -n AC --obs_type head -r dose -e 100 2500 --fname dose > training_logs/cani/dose
+./main.py --canicula -n AC --obs_type head -r dose -e 70 2500 --fname dose > training_logs/cani/dose
