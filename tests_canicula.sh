47ca9f208f8d0bc7129f9bda81b8500146b32e5b
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-06-02 19:51:13 +0200

    tests on canicula

--- tests_canicula.sh
+++ tests_canicula.sh
@@ -3,4 +3,4 @@
 mkdir -p training_logs/ca_types_dose
 
 #types dose
-./main.py --canicula -n AC --obs_type types -r dose --fname ac_dose_ult -e 200 2500 -s c++ -l 0.005 .75 4 > training_logs/ca_types_dose/dose_0.005-.75-4
+./main.py --canicula -n AC --obs_type types -r dose --fname ac_dose_ult -e 120 2500 -s c++ -l 0.005 .75 4 > training_logs/ca_types_dose/dose_0.005-.75-4
