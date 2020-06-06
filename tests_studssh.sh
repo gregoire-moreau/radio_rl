5eeb2bab08496cfa4bb172dc824842be2fb5b0fc
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-06-06 04:46:14 +0200

    tests on vm

--- tests_studssh.sh
+++ tests_studssh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir -p training_logs/st_killed
+mkdir -p training_logs/st_killednosp
 
 #types killed
-./main.py -n DQN --obs_type types -r killed  -e 100 2500 -s c++ -l 0.01 0.9 1 --fname st_killed_sp > training_logs/st_killed/killed-sp
-./main.py -n DQN --obs_type types -r killed  -e 100 2500 -s c++ -l 0.01 0.9 1 --no-special --fname st_killed_no > training_logs/st_killed/killed-nosp
+./main.py -n DQN --obs_type types -r killed  -e 100 2500 -s c++ -l 0.01 0.9 1 --no_special --fname st_killed_no > training_logs/st_killednosp/killed-nosp
