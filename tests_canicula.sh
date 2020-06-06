1302c5110a78139e38279c74d1c019509d843969
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-06-06 04:55:04 +0200

    test cani

--- tests_canicula.sh
+++ tests_canicula.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-mkdir -p training_logs/ca_killed
-
-#types dose
-./main.py --canicula -n AC --obs_type types -r killed  -e 100 2500 -s c++ -l 0.001 0.9 1 --fname ca_killed_sp > training_logs/ca_killed/killed-sp
-./main.py --canicula -n AC --obs_type types -r killed  -e 100 2500 -s c++ -l 0.001 0.9 1 --no-special --fname ca_killed_no > training_logs/ca_killed/killed-nosp
+mkdir -p training_logs/ca_killed_nosp
+./main.py --canicula -n AC --obs_type types -r killed  -e 100 2500 -s c++ -l 0.001 0.9 1 --no_special --fname ca_killed_no > training_logs/ca_killed_nosp/killed-nosp
