38ef4f0060d9b58008ccd67c6eae40c6b30209f9
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-06-10 17:33:40 +0200

    new tests

--- tests_canicula.sh
+++ tests_canicula.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p training_logs/killed2
+mkdir -p training_logs/killed_ac
 
-./main.py --canicula -n DQN --obs_type types -r killed  -e 100 2500 -s c++ -l 0.01 0.9 1 --fname killed2 > training_logs/killed2/killed
+./main.py --canicula -n AC --obs_type types -r killed  -e 200 2500 -s c++ -l 0.005 0.9 1 --fname killed_ac > training_logs/killed_ac/killed
--- tests_studssh.sh
+++ tests_studssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p training_logs/killed1
+mkdir -p training_logs/killed4
 
-./main.py -n DQN --obs_type types -r killed  -e 100 2500 -s c++ -l 0.01 0.9 1 --fname killed1 > training_logs/killed1/killed
+./main.py -n DQN --obs_type types -r killed  -e 200 2500 -s c++ -l 0.01 0.9 1 --fname killed4 > training_logs/killed4/killed
