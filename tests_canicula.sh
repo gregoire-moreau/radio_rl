472b34bf21c9ab0f69593f2169a97745cb299557
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-06-12 04:51:48 +0200

    killed ac

--- tests_canicula.sh
+++ tests_canicula.sh
@@ -2,4 +2,4 @@
 
 mkdir -p training_logs/dose_ac
 
-./main.py --canicula -n AC --obs_type types -r killed  -e 200 2500 -s c++ -l 0.01 0.9 1 --fname dose_ac > training_logs/dose_ac/dose
+./main.py --canicula -n AC --obs_type types -r dose  -e 200 2500 -s c++ -l 0.01 0.9 1 --fname dose_ac > training_logs/dose_ac/dose
