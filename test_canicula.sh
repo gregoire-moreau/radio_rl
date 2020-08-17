94b883e4a2fef6c3ddb56a867791042d749067d7
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-08-17 12:46:37 +0200

    test dqn

--- test_canicula.sh
+++ test_canicula.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir -p training_logs/killed_new
+mkdir -p training_logs/dose_new
 
-./main.py --canicula -n DQN --obs_type densities -r killed  -e 200 2500 -s c++ -l 0.01 0.9 1 --fname killed_new > training_logs/killed_new
+./main.py --canicula -n DQN --obs_type densities -r dose  -e 200 2500 -s c++ -l 0.01 0.9 1 --fname dose_new > training_logs/dose_new/dose
+
