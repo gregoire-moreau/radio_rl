6e587d403a9909642c6ee104f9b2fe2088603096
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-08-18 08:54:19 +0200

    add killed_4

--- test_vm.sh
+++ test_vm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./model_cpp/main 50 4 i 50 5 killed_small.csv > killed_small
+./model_cpp/main 50 d i 50 5 dose_small.csv > dose_smal
+./model_cpp/main 50 1 i 50 5 k1.csv > k1
+./model_cpp/main 50 2 i 50 5 k2.csv > k2
+./model_cpp/main 50 4 i 50 5 k4.csv > k4
