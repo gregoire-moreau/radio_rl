cf14eb4682333efe0128380bb2e1d7dca480ac1e
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-08-25 13:25:02 +0200

    Dose ddpg new

--- test_canicula.sh
+++ test_canicula.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p training_logs/dose_new
+mkdir -p training_logs/dose_ddpg_new
 
-./main.py --canicula -n DQN --obs_type densities -r dose  -e 100 2500 -s c++ -l 0.0005 0.9 1 --fname dose_new > training_logs/dose_new/dose
+./main.py --canicula -n DDPG --obs_type densities -r dose  -e 100 2500 -s c++ -l 0.001 0.9 1 --fname dose_ddpg_new4 > training_logs/dose_ddpg_new/dose4
 
-./main.py --canicula -n DQN --obs_type densities -r dose  -e 100 2500 -s c++ -l 0.0005 0.9 1 --fname dose_new3 > training_logs/dose_new/dose3
+./main.py --canicula -n DDPG --obs_type densities --exploration gauss -r dose  -e 100 2500 -s c++ -l 0.001 0.9 1 --fname dose_ddpg_new2 > training_logs/dose_ddpg_new/dose2
 
-./main.py --canicula -n DQN --obs_type densities -r dose  -e 100 2500 -s c++ -l 0.0005 0.9 1 --fname dose_new4 > training_logs/dose_new/dose4
+./main.py --canicula -n DDPG --obs_type densities --exploration gauss -r dose  -e 100 2500 -s c++ -l 0.0001 0.9 1 --fname dose_ddpg_new3 > training_logs/dose_ddpg_new/dose3
 
