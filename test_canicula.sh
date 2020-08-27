f717508d7869508f3970d9343a750133e3431fa0
Grégoire Moreau <gregoire.moreau@student.uclouvain.be>
2020-08-27 11:22:29 +0200

    test

--- test_canicula.sh
+++ test_canicula.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+./use_network.py  -n DDPG -r dose --fname dose_ddpg_n > eval_dose_ddpg
 
-mkdir -p training_logs/dose_ddpg_new
+./use_network.py  -n DDPG -r dose --fname dose_ddpg_2 > eval_dose_ddpg2
 
-./main.py --canicula -n DDPG --obs_type densities -r dose  -e 100 2500 -s c++ -l 0.001 0.9 1 --fname dose_ddpg_new4 > training_logs/dose_ddpg_new/dose4
+./use_network.py  -n DDPG -r dose --fname dose_ddpg_3 > eval_dose_ddpg3
 
-./main.py --canicula -n DDPG --obs_type densities --exploration gauss -r dose  -e 100 2500 -s c++ -l 0.001 0.9 1 --fname dose_ddpg_new2 > training_logs/dose_ddpg_new/dose2
-
-./main.py --canicula -n DDPG --obs_type densities --exploration gauss -r dose  -e 100 2500 -s c++ -l 0.0001 0.9 1 --fname dose_ddpg_new3 > training_logs/dose_ddpg_new/dose3
+./use_network.py  -n DDPG -r dose --fname dose_ddpg_4 > eval_dose_ddpg4
 
