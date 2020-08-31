58b3d1d9fedfff3b435d9a4bacdff5e8acce8e6b
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2020-08-31 17:34:22 +0200

    cleaning

--- test_canicula.sh
+++
@@ -1,8 +0,0 @@
-./use_network.py  -n DDPG -r dose --fname dose_ddpg_n > eval_dose_ddpg
-
-./use_network.py  -n DDPG -r dose --fname dose_ddpg_2 > eval_dose_ddpg2
-
-./use_network.py  -n DDPG -r dose --fname dose_ddpg_3 > eval_dose_ddpg3
-
-./use_network.py  -n DDPG -r dose --fname dose_ddpg_4 > eval_dose_ddpg4
-
